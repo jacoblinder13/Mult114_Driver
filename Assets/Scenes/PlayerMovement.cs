2b4869ad62cf84168d319c2dc372ccfc4e47df30
Jake Linder <jacoblinder@yahoo.com>
2020-02-10 10:44:09 -0800

    Initial

---
+++ Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Here is my comment
+        transform.Translate(Vector3.forward * Time.deltaTime * 20 * speed);
+    }
+}
